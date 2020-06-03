50233da2565f6c7dc9a2bfa5755913c4bd412c65
Johannes Wehner <johw@dr.dk>
2020-06-03 08:25:12 +0200

    Responsive

--- src/script/components/audio-quote/audio-quote.js
+++ src/script/components/audio-quote/audio-quote.js
@@ -6,13 +6,25 @@ import './audio-quote.scss';
 
 export default class AudioQuote {
     constructor() {
-        this.container = document.querySelector('[data-audio-quote] .audio-quote-widget-container');
+        this.element  = document.querySelector('[data-audio-quote]');
+
+
+        this.container = this.element.querySelector('.audio-quote-widget-container');
+
 
         if (!this.container) {
             console.error('missing audio-quote container')
         }
 
-        this.file = this.container.dataset.audioQuote;
+
+        this.file = this.element.dataset.audioQuote;
+        console.log()
+        if (!this.file) {
+            console.error('missing audio-quote file')
+        }
+
+        this.playing = false;
+
         this.build();
     }
 
@@ -22,11 +34,117 @@ export default class AudioQuote {
 
         this.container.appendChild(this.canvas)
 
-        this.visualizer = new Visualizer(this.canvas);
+        this.visualizer = new Visualizer(this.canvas, this.element);
 
+        this.audio = document.createElement('audio');
+        this.audio.setAttribute('preload', 'auto')
+        this.audio.setAttribute('loop', true)
+        this.audio.src = this.file;
+
+        this.audio.addEventListener('canplaythrough', () => {
+            console.log('loaded')
+            this.setLoaded()
+        }, false);
+
+        window.addEventListener('resize',  () => {
+
+
+            // only run if we're not throttled
+            /*if (!this.throttled) {
+                // actual callback action
+                this.handleResize();
+                // we're throttled!
+                this.throttled = true;
+                // set a timeout to un-throttle
+                setTimeout(function () {
+                    this.throttled = false;
+                }, 250);
+            }*/
+            clearTimeout(this.timeout);
+            // start timing for event "completion"
+            this.timeout = setTimeout(() => {
+                this.handleResize();
+            }, 200);
+        });
+
+        this.toggleButton = document.createElement('button')
+        this.toggleButton.id = 'toggle-button'
+        this.toggleButton.innerHTML = `
+            <svg version="1.1" id="sound" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
+            <style type="text/css">
+                .bg{fill:#FF001E;}
+                .color{fill:#FFFFFF;}
+            </style>
+            <g>
+                <g>
+                    <circle class="bg" cx="25.2" cy="25.2" r="25"/>
+                </g>
+                <g>
+                    <polygon class="color" points="16.7,21.2 12.7,21.2 12.7,29.3 16.7,29.3 23.2,34 23.2,16.5 		"/>
+                    <path class="color mute" d="M28.1,33.2l-1.7-1.7c3.4-3.5,3.4-9,0-12.5l1.7-1.7c2.1,2.1,3.3,5,3.3,8S30.3,31.1,28.1,33.2z"/>
+                    <path class="color mute" d="M32.8,37.3L31,35.5c5.6-5.7,5.6-14.9,0-20.6l1.7-1.7C39.3,19.9,39.3,30.6,32.8,37.3z"/>
+
+                    <polygon  class="color sound" points="38.5,21.9 36.8,20.2 33.2,23.8 29.6,20.2 27.9,21.9 31.5,25.5 27.9,29.1 29.6,30.8 33.2,27.2
+                        36.8,30.8 38.5,29.1 34.9,25.5 		"/>
+                </g>
+            </g>
+            </svg>
+
+        `;
+
+        this.controls = document.createElement('div');
+        this.controls.id = 'controls'
+
+        this.progress = document.createElement('div');
+        this.progress.id = 'progress'
+
+        this.element.appendChild(this.controls)
+
+        this.controls.appendChild(this.audio)
+        this.controls.appendChild(this.progress)
+        this.controls.appendChild(this.toggleButton)
         console.log(this.file)
 
 
+    }
+
+    handleResize() {
+        console.log('handle r5esize')
+        this.visualizer.setDimensions(this.element)
+    }
+    setLoaded() {
+
+
+        this.updateProgress()
+
+        this.toggleButton.addEventListener('click', () => {
+            if (this.playing) {
+                this.audio.pause()
+            } else {
+                this.audio.play()
+            }
+            this.playing = !this.playing;
+            console.log('Palying : ' + this.playing)
+            this.toggleButton.classList.toggle("playing", this.playing);
+        })
+
+    }
+    setProgress() {
+        let time = Math.round(this.audio.duration - this.audio.currentTime).toString();
+        this.time = (time.length < 2) ? '0' + time : time;
+        if (this.time != this.currentTime) {
+            this.progress.innerText = `00:${this.time}`;
+            this.currentTime = this.time
+        }
+    }
+    updateProgress() {
+
+        this.setProgress()
+        requestAnimationFrame(() => {
+            //this.progress
+            this.updateProgress();
+        })
     }
 
 }
--- src/script/components/visualizer/visualizer.js
+++ src/script/components/visualizer/visualizer.js
@@ -1,54 +1,86 @@
+const color = '#ffe1f9';
+const bg = '#fff';
 
 export default class Visualizer {
 
-  constructor(element) {
+  constructor(element, container) {
 
     this.canvas = element;
-    this.canvas.width = window.innerWidth;
-    this.canvas.height = window.innerHeight;
+    this.canvas.width = container.offsetWidth;
+    this.canvas.height = Math.max(container.offsetHeight, 100);
     //console.log(this.canvas);
     this.canvasContext = this.canvas.getContext("2d");
     this.lowerFrequencyStop = 8;
-    this.defaultRandomWaveFactor = 50;
+    this.defaultRandomWaveFactor = 30;
     this.screenHeightWaveFactor = Math.min(window.innerHeight/900, 1);
     this.waveFactor = 1; //Math.min(window.innerWidth/window.innerHeight, 1); //0.8;
-
+    this.noPoints = 32; //128;
     //console.log('WF: ' + this.waveFactor);
     this.points = [];
+    this.initialPoints = [];
+    this.initialFactors = [];
+    this.count = 0;
 
     this.animate()
   }
-
+  setDimensions(container) {
+    this.canvas.width = container.offsetWidth;
+    this.canvas.height = Math.max(container.offsetHeight, 100);
+  }
   animate() {
-
+      this.count++;
       this.updateWave()
       requestAnimationFrame(() => {
         this.animate();
       })
   }
+  clamp(initial, y) {
 
+
+    const newY = Math.min(Math.max(y, initial), initial + 20);
+    //console.log(newY, this.canvas.height/2);
+    return Math.min(newY, this.canvas.height / 2);
+  }
   updateWave() {
 
 
     this.canvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
     this.canvasContext.beginPath();
-    this.canvasContext.strokeStyle= '#fff';
+    this.canvasContext.strokeStyle= color;
     this.canvasContext.lineWidth= 1;
 
+    this.goingUp = (this.count % (200  )) > 100;
 
+    //console.log(this.flat)
 
     // BOTTOM
     this.canvasContext.moveTo(0, this.canvas.height);
 
     let x = 0;
-    for (var i = 0; i < 128; i++) {
+    for (var i = 0; i <= this.noPoints; i++) {
+
+
 
       //var v = dataArrayAlt[i] / 128.0;
-      var y = (this.canvas.height)  - Math.floor(Math.random() * this.defaultRandomWaveFactor * this.waveFactor)
-      //var y = this.canvas.height -
-      this.canvasContext.lineTo(x, y);
-      x += Math.floor((this.canvas.width / 128) * 2);
+      //var y = (this.canvas.height)  - Math.floor(Math.random() * this.defaultRandomWaveFactor * this.waveFactor)
+      const sd = i / (this.noPoints / 2)
+      const factor = Math.abs(1 - sd);
+      if (!this.points[i]) {
+        var y = (factor * 180) + (this.canvas.height / 24)
+        y -= (Math.random() * this.defaultRandomWaveFactor);
+        this.initialPoints[i] = y;
+        this.initialFactors[i] = factor;
+        this.points[i] = y;
+      }
+
+      this.points[i] = (this.points[i] * (this.goingUp ? 1.0005 : 0.9995)) + ((Math.random() - 0.5) * 12)
+      this.points[i] = this.clamp(this.initialPoints[i], this.points[i])
+
+
+
+      this.canvasContext.lineTo(x, this.points[i]);
+      x += Math.ceil((this.canvas.width / this.noPoints));
 
     }
     this.canvasContext.lineTo(this.canvas.width,this.canvas.height);
@@ -58,7 +90,7 @@ export default class Visualizer {
     this.canvasContext.stroke();
     this.canvasContext.closePath();
     this.canvasContext.stroke();
-    this.canvasContext.fillStyle="#fff";
+    this.canvasContext.fillStyle = color;
     this.canvasContext.fill();
 
 
@@ -66,12 +98,14 @@ export default class Visualizer {
 
     this.canvasContext.moveTo(0, 0);
     x = 0;
-    for (var i = 0; i < 128; i++) {
+    for (var i = 0; i <= 128; i++) {
 
       //var v = dataArrayAlt[i] / 128.0;
-      var y = Math.floor(Math.random() * this.defaultRandomWaveFactor * this.waveFactor)
+      //var y = Math.floor(Math.random() * this.defaultRandomWaveFactor * this.waveFactor)
+      var y = this.canvas.height - this.points[i];
+
       this.canvasContext.lineTo(x, y);
-      x += Math.floor((this.canvas.width / 128) * 2);
+      x += Math.ceil((this.canvas.width / this.noPoints));
 
     }
     this.canvasContext.lineTo(this.canvas.width, 0);
@@ -81,78 +115,11 @@ export default class Visualizer {
     this.canvasContext.stroke();
     this.canvasContext.closePath();
     this.canvasContext.stroke();
-    this.canvasContext.fillStyle="#fff";
+    this.canvasContext.fillStyle = bg;
     this.canvasContext.fill();
 
 
 
   }
 
-  updateVisual(analyser) {
-
-    analyser.fftSize = 256;
-    var bufferLengthAlt = analyser.frequencyBinCount;
-
-    var dataArrayAlt = new Uint8Array(bufferLengthAlt);
-
-    this.canvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-    this.canvasContext.beginPath();
-    analyser.getByteFrequencyData(dataArrayAlt);
-    this.canvasContext.strokeStyle= '#fff';
-    /*
-    this.canvasContext.fillStyle = '#fff';
-    this.canvasContext.fillRect(0, 0, this.canvas.width, this.canvas.height);*/
-
-    this.canvasContext.lineWidth= 1;
-
-    // BOTTOM
-    this.canvasContext.moveTo(0, this.canvas.height);
-
-    let x = 0;
-    for (var i = this.lowerFrequencyStop; i < bufferLengthAlt; i++) {
-
-      //var v = dataArrayAlt[i] / 128.0;
-      var y = (this.canvas.height)  - Math.floor(dataArrayAlt[i]*this.waveFactor*this.screenHeightWaveFactor)
-      this.canvasContext.lineTo(x, y);
-      x += Math.floor((this.canvas.width / bufferLengthAlt) * 2);
-    }
-    this.canvasContext.lineTo(this.canvas.width,this.canvas.height);
-    this.canvasContext.lineTo(0, this.canvas.height);
-
-    //this.canvasContext.lineTo(this.canvas.width, this.canvas.height / 2);
-    this.canvasContext.stroke();
-    this.canvasContext.closePath();
-    this.canvasContext.stroke();
-    this.canvasContext.fillStyle="#fff";
-    this.canvasContext.fill();
-
-
-
-    // TOP
-    this.canvasContext.moveTo(0, 0);
-    x = 0;
-    for (var i = this.lowerFrequencyStop; i < bufferLengthAlt; i++) {
-
-      //var v = dataArrayAlt[i] / 128.0;
-      var y =  Math.floor(dataArrayAlt[i]*this.waveFactor*this.screenHeightWaveFactor)
-      this.canvasContext.lineTo(x, y);
-      x += Math.floor((this.canvas.width / bufferLengthAlt) * 2);
-
-    }
-    this.canvasContext.lineTo(this.canvas.width, 0);
-    this.canvasContext.lineTo(0, 0);
-
-    //this.canvasContext.lineTo(this.canvas.width, this.canvas.height / 2);
-    this.canvasContext.stroke();
-    this.canvasContext.closePath();
-    this.canvasContext.stroke();
-    this.canvasContext.fillStyle="#fff";
-    this.canvasContext.fill();
-
-
-
-
-
-  }
 }
